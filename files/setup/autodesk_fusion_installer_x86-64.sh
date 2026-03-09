48eb3c5c6a492fb3d6b7517de48873809fb6e7c4
Lolig4 <Urias.Uhle@web.de>
2026-03-09 13:54:25 +0100

    deleting braking .desktop file

--- files/setup/autodesk_fusion_installer_x86-64.sh
+++ files/setup/autodesk_fusion_installer_x86-64.sh
@@ -1222,6 +1222,7 @@ autodesk_fusion_shortcuts_load() {
 
     local SCHORTCUT_DIRECTORY="$DESKTOP_DIRECTORY/$NEW_ID"
     mkdir -p "$SCHORTCUT_DIRECTORY"
+    rm -f "$DESKTOP_DIRECTORY/Autodesk Fusion.desktop"
 
     echo "$SELECTED_DIRECTORY" >> "$SCHORTCUT_DIRECTORY/location.log"
     chmod 444 "$SCHORTCUT_DIRECTORY/location.log"
